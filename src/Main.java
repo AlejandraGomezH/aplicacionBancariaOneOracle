deb881657162f8c3ba372e759de15d444d2992bd
AlejandraGomezH <93231057+AlejandraGomezH@users.noreply.github.com>
2024-10-17 23:19:12 -0600

    Segundo commit para la aplicación bancaria en el curso de alura latam one oracle education

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner teclado = new Scanner(System.in);
+        String nombreCliente = "Tony Stark";
+        String tipoCuenta = "Corriente";
+        int saldoDisponible = 0;
+        int opcion = 0;
+
+            System.out.println("*************************");
+            System.out.println("Nombre del cliente: " + nombreCliente);
+            System.out.println("Tipo de cuenta: " + tipoCuenta);
+            System.out.println("Saldo disponible: " + saldoDisponible);
+            System.out.println("*************************");
+
+        while (opcion == 9){
+            System.out.println("** Escriba el número de la opción deseada **");
+            System.out.println("1. Consultar");
+            System.out.println("2. Retorar");
+            System.out.println("3. Depositar");
+            System.out.println("9. Salir");
+            opcion = teclado.nextInt();
+
+            if (opcion == 1){
+                System.out.println("El saldo actualizado es: " + saldoDisponible);
+            }
+        }
+
+
     }
 }
